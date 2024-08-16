8467eb5c2ee4a9f76a4062a56678c48709df0003
Matthew Allison <matthewallisonxyz@gmail.com>
2024-08-16 12:35:41 +0200

    slit text into lines

--- lua/goosechat.lua
+++ lua/goosechat.lua
@@ -15,10 +15,14 @@ function M.copy_down()
 
 	-- Get the selected text
 	local lines = vim.api.nvim_buf_get_lines(0, start_row, end_row, false)
-	local selected_text = table.concat(lines, "\n")
+
+	-- If the selection spans multiple lines, handle the partial line at the end
+	if end_col > 0 and end_col < #lines[#lines] then
+		lines[#lines] = lines[#lines]:sub(1, end_col)
+	end
 
 	-- Insert the selected text one line down
-	vim.api.nvim_buf_set_lines(0, end_row + 1, end_row + 1, false, { selected_text })
+	vim.api.nvim_buf_set_lines(0, end_row + 1, end_row + 1, false, lines)
 end
 
 return M
